a703c88654317a850f8f89ddaadfcc10465cb92d
teryzwiak <52777142+teryzwiak@users.noreply.github.com>
2020-12-20 20:20:49 +0100

    town

--- search_engine/town.php
+++ search_engine/town.php
@@ -1,3 +1,35 @@
+
 <?php
-echo "test";
+require_once "../db.php";
+$con = @new mysqli($host, $db_user, $db_password, $db_name);
+if($con->connect_errno!=0)
+{
+    echo $con->connect_errno;
+}
+else{
+$query = "SELECT data FROM wypadki";
+if($result = @$con->query($query))
+{
+$how = $result->num_rows;
+if($how>0)
+{
 ?>
+<form action="search_date.php" method="post">
+    <label for="date">Wybierz datę:</label>
+
+    <?php
+    echo "<select name='date'>";
+    while($date = $result->fetch_row())
+    {
+        echo "<option value='$date[0]'>".$date[0]."</option>";
+    }
+    echo "</select>";
+    }
+    }
+    }
+    $con->close();
+    ?>
+
+    <input type="submit" value="Szukaj">
+</form>
+
