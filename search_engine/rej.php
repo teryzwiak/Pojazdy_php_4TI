5bd893713ed902d6ed38925adb2a8d34c2cfb896
teryzwiak <52777142+teryzwiak@users.noreply.github.com>
2020-12-20 20:27:46 +0100

    rej

--- search_engine/rej.php
+++ search_engine/rej.php
@@ -1,1 +1,35 @@
+
 <?php
+require_once "../db.php";
+$con = @new mysqli($host, $db_user, $db_password, $db_name);
+if($con->connect_errno!=0)
+{
+    echo $con->connect_errno;
+}
+else{
+$query = "SELECT rejestracja FROM wypadki";
+if($result = @$con->query($query))
+{
+$how = $result->num_rows;
+if($how>0)
+{
+?>
+<form action="search_date.php" method="post">
+    <label for="rej">Wybierz rejestracje:</label>
+
+    <?php
+    echo "<select name='rej'>";
+    while($date = $result->fetch_row())
+    {
+        echo "<option value='$date[0]'>".$date[0]."</option>";
+    }
+    echo "</select>";
+    }
+    }
+    }
+    $con->close();
+    ?>
+
+    <input type="submit" value="Szukaj">
+</form>
+
--- search_engine/town.php
+++ search_engine/town.php
@@ -7,18 +7,18 @@ if($con->connect_errno!=0)
     echo $con->connect_errno;
 }
 else{
-$query = "SELECT data FROM wypadki";
+$query = "SELECT miejsce FROM wypadki";
 if($result = @$con->query($query))
 {
 $how = $result->num_rows;
 if($how>0)
 {
 ?>
-<form action="search_date.php" method="post">
-    <label for="date">Wybierz datę:</label>
+<form action="search_town.php" method="post">
+    <label for="date">Wybierz miejsce:</label>
 
     <?php
-    echo "<select name='date'>";
+    echo "<select name='miejsce'>";
     while($date = $result->fetch_row())
     {
         echo "<option value='$date[0]'>".$date[0]."</option>";
