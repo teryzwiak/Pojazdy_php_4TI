7da3bd0a9b647e641af9c33acac7628858093721
teryzwiak <52777142+teryzwiak@users.noreply.github.com>
2021-01-09 20:12:33 +0100

    style new

--- loginpage.php
+++ loginpage.php
@@ -12,7 +12,7 @@ session_start();
 <body>
 
 <div class="topnav">
-    <p><strong>Baza wypadków</strong></p>
+    <p><strong><a href="index.php">Baza wypadków</a></strong></p>
     <a href="#">Kontakt</a>
     <a href="#">Zarejestruj się</a>
     <a href="loginpage.php">Zaloguj się</a>
@@ -20,7 +20,8 @@ session_start();
 
 <div class="content">
     <div class="column-left">
-        <h2 class="login-banner"> Zaloguj się by przejść do panelu</h2>
+        <div class="form-content">
+        <h2 class="login-banner"> Zaloguj się:</h2>
         <form action="login.php" method="post" class="form">
             <label for="login"><strong>Login:</strong> </label><br>
             <input type="text" name="login"> <br/><br/>
@@ -29,11 +30,13 @@ session_start();
             <input type="submit" value="Zaloguj się">
         </form>
         <?php if(isset($_SESSION['blad']))echo $_SESSION['blad'];?>
+        </div>
+    </div>
 
-
-
+    <div class="column-right">
         <img src="assets/car-accident.jpg" alt="wypadek" style="width: 60%" width="100%" height="100%" >
     </div>
 </div>
+
 </body>
 </html>
