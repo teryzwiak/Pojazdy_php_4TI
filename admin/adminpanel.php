c06e4d3712d6d9f8c8af113a5585a247bafda680
teryzwiak <52777142+teryzwiak@users.noreply.github.com>
2021-01-09 21:20:23 +0100

    adminpanel.php

--- admin/adminpanel.php
+++ admin/adminpanel.php
@@ -1,1 +1,29 @@
 <?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title>Pojazdy</title>
+    <link rel="stylesheet" href="csstyle.css" >
+</head>
+<body>
+
+<div class="topnav">
+    <p><strong><a href="index.php">BAZA WYPADKÓW</a></strong></p>
+    <a href="#">Zarejestruj się</a>
+    <a href="loginpage.php">Zaloguj się</a>
+</div>
+
+<div class="content">
+
+</div>
+
+
+
+
+
+</body>
+</html>
