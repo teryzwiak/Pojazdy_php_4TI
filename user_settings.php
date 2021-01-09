e380c91f0ab6e587ab038a5feeff363b84df5ace
teryzwiak <52777142+teryzwiak@users.noreply.github.com>
2021-01-09 20:52:24 +0100

    style new

--- user_settings.php
+++ user_settings.php
@@ -7,31 +7,41 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <title>Pojazdy</title>
-    <link rel="stylesheet" href="styl1.css" >
+    <link rel="stylesheet" href="csstyle.css" >
 </head>
 <body>
-<div class="header"><br/>
-    <h1>Baza pojazdów i ich wypadki</h1> <br/>
+
+<div class="topnav">
+    <p><strong><a href="index.php">BAZA WYPADKÓW</a></strong></p>
+    <a href="#">Zarejestruj się</a>
+    <a href="loginpage.php">Zaloguj się</a>
 </div>
-<div class="hub">
-    Zalogowano jako: <?php echo $_SESSION['login'];?><br/>
-    Twój email to: <?php echo $_SESSION['email'];?><br/>
-    Email jawny: <?php echo $_SESSION['jawny'];?><br/>
-    Dodaj wypadek: <a href="/admin/db_modification.php">LINK</a>
-</div>
-<div class="content-left">
-    <h2>Znajdź wypadek/pojazd:</h2>
-    <ul>
-       <li><a href="search_engine/date.php" target="iframe">Szukaj po dacie</a></li>
-        <li><a href="search_engine/town.php" target="iframe">Szukaj po miejscowosci</a></li>
-        <li><a href="search_engine/rej.php" target="iframe">Szukaj po rejestracji</a></li>
-    </ul>
+
+<div class="content">
+    <div class="hub">
+        Zalogowano jako: <?php echo $_SESSION['login'];?><br/>
+        Twój email to: <?php echo $_SESSION['email'];?><br/>
+        Email jawny: <?php echo $_SESSION['jawny'];?><br/>
+        Dodaj wypadek: <a href="/admin/db_modification.php">LINK</a>
+    </div>
+    <div class="content-left">
+        <h2>Znajdź wypadek/pojazd:</h2>
+        <ul>
+            <li><a href="search_engine/date.php" target="iframe">Szukaj po dacie</a></li>
+            <li><a href="search_engine/town.php" target="iframe">Szukaj po miejscowosci</a></li>
+            <li><a href="search_engine/rej.php" target="iframe">Szukaj po rejestracji</a></li>
+        </ul>
+
+    </div>
+    <div class="content-right-s" >
+
+        <iframe src="test.php" name="iframe" width="100%" height="100%"></iframe>
+    </div>
 
 </div>
-<div class="content-right-s" >
 
-    <iframe src="test.php" name="iframe" width="100%" height="100%"></iframe>
-</div>
+
+
 
 </body>
 </html>
