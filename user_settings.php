bc08bd7495383605cde00f8f7bab76e2054a5186
teryzwiak <52777142+teryzwiak@users.noreply.github.com>
2021-01-10 21:52:07 +0100

    adminpanel.php

--- user_settings.php
+++ user_settings.php
@@ -13,29 +13,29 @@ session_start();
 
 <div class="topnav">
     <p><strong><a href="index.php">BAZA WYPADKÓW</a></strong></p>
-    <a href="#">Zarejestruj się</a>
-    <a href="loginpage.php">Zaloguj się</a>
+    <a href="#">Zalogowano jako: <?php echo $_SESSION['login'];?></a>
+
 </div>
 
 <div class="content">
     <div class="hub">
-        Zalogowano jako: <?php echo $_SESSION['login'];?><br/>
         Twój email to: <?php echo $_SESSION['email'];?><br/>
         Email jawny: <?php echo $_SESSION['jawny'];?><br/>
         Dodaj wypadek: <a href="admin/adminpanel.php">LINK</a>
     </div>
-    <div class="content-left">
+    <br/><br/>
+    <div class="column-left">
+        <div class="menu">
         <h2>Znajdź wypadek/pojazd:</h2>
         <ul>
             <li><a href="search_engine/date.php" target="iframe">Szukaj po dacie</a></li>
             <li><a href="search_engine/town.php" target="iframe">Szukaj po miejscowosci</a></li>
             <li><a href="search_engine/rej.php" target="iframe">Szukaj po rejestracji</a></li>
         </ul>
-
+        </div>
     </div>
-    <div class="content-right-s" >
-
-        <iframe src="test.php" name="iframe" width="100%" height="100%"></iframe>
+    <div class="column-right">
+        <iframe src="test.php" name="iframe" width="100%" height="400px"></iframe>
     </div>
 
 </div>
