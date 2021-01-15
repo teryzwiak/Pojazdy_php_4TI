650778c94e7731c982128af58626f12eb51439a1
teryzwiak <52777142+teryzwiak@users.noreply.github.com>
2021-01-15 16:49:44 +0100

    fn

---
+++ search_engine/search.php
@@ -0,0 +1,32 @@
+<?php
+function search($data, $result)
+{
+    echo "Data:".$data;
+    echo "<table border='1' bgcolor='#7fffd4'>";
+    echo "<tr><td> rejestracja </td> <td> czy winny </td> <td> poniesiona strata </td><td> miejsce wypadku </td> </tr>";
+    while($odp = $result->fetch_row())
+    {
+        echo "<td>".$odp[2]."</td><td>".$odp[3]."</td><td>".$odp[4]."</td><td>".$odp[5]."</td></tr>";
+    }
+    echo "</table>";
+}
+
+require_once "../db.php";
+$con = @new mysqli($host, $db_user, $db_password, $db_name);
+if($con->connect_errno!=0)
+{
+    echo $con->connect_errno;
+}
+else
+{
+    $date = $_POST['date'];
+    $query = "SELECT * FROM wypadki WHERE data='$date'";
+    if($result = @$con->query($query)){
+        $how=$result->num_rows;
+        if($how>0)
+        {
+            search($date, $result);
+        }
+    }
+}
+?>
